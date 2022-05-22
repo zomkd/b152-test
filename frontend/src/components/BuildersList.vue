66b278ab42bf06bf5c62ddced2986ea14781fa2c
dull69 <dull69@mail.ru>
2022-05-22 22:19:04 +0300

    add style for builder list

--- frontend/src/components/BuildersList.vue
+++ frontend/src/components/BuildersList.vue
@@ -1,19 +1,25 @@
 <template>
-  <section class="main">
-    <h1 class="text-3xl font-bold underline">
-    Привет мир!
-  </h1>
-    <ul class="builders-list">
-      <li class="builder-item" v-for="builder in builders" :key="builder.name">
+<div class="flex flex-col justify-center items-center">
+  <section class="main w-full bg-white rounded-lg shadow-lg lg:w-1/3">
+    <ul class="builders-list divide-y-2 divide-gray-100">
+      <li
+        class="p-3 hover:bg-blue-600 hover:text-blue-200"
+        v-for="builder in builders"
+        :key="builder.name"
+      >
         <router-link
+          class="flex justify-between"
           :to="{ name: 'builderCard', params: { name: builder.name } }"
         >
           {{ builder.name }}
-          {{ builder.houses }}
+          <div class="houses-count flex font-bold">
+            Количество построенных домов: <div class="font-medium ml-5"> {{ builder.houses }}</div>
+          </div>
         </router-link>
       </li>
     </ul>
   </section>
+  </div>
 </template>
 
 <script>
